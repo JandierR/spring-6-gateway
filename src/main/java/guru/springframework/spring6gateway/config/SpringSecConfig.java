cd3d549652c535ef05ad5804b9799b2a12cfbd3b
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-05-18 11:11:11 -0600

    Some changes

--- src/main/java/guru/springframework/spring6gateway/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6gateway/config/SpringSecConfig.java
@@ -1,7 +1,9 @@
 package guru.springframework.spring6gateway.config;
 
+import org.springframework.boot.security.autoconfigure.actuate.web.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -10,6 +12,15 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class SpringSecConfig {
+
+    @Bean
+    @Order(1)
+    public SecurityWebFilterChain actutatorSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeExchange(authorize -> authorize.anyExchange().permitAll());
+
+        return http.build();
+    }
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
